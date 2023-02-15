ce69fc25b94a52e62d3c14679ec62e553f1e5ab7
vin <didekqls93@gmail.com>
2023-02-15 13:17:55 +0900

    feat: create button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+const editItem = () => {};
+
+const deleteItem = () => {};
 function TodoItem(props) {
-  return <div className="todo-list">{props.item}</div>;
+  return (
+    <div>
+      <div className="todo-list">{props.item}</div>
+      <button onClick={editItem}>Edit</button>
+      <button onClick={deleteItem}>Delete</button>
+    </div>
+  );
 }
 
 export default TodoItem;
